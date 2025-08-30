ad319ed629e0c0538bf1db93c949eff021c544df
HenriqueGomes <henriquegomesjunqueira@gmail.com>
2025-08-30 20:48:12 -0300

    Implement database models and mapping functions for vulnerabilities

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client["cve_database"]
+modelo1 = client["modelo1"]
+modelo2 = client["modelo2"]
+
+vulnerabilities_collection = modelo1["vulnerabilities"]
--- main.py
+++ main.py
@@ -5,6 +5,3 @@ cve_id = "CVE-2021-34527"
 cve_data = get_cve(cve_id)
 epss_data = get_epss(cve_id)
 
-print("CVE Data:", cve_data.get("baseScore"))
-print("EPSS Data:", epss_data.get("epss"))
-
