e536fd427c78874f45ccd1eff685037dc726b356
HenriqueGomes <henriquegomesjunqueira@gmail.com>
2025-08-31 02:05:19 -0300

    Refactor enchance_data function to use a query for distinct CVE IDs and ensure default values for CVSS and EPSS updates

--- enchance_data.py
+++ enchance_data.py
@@ -3,8 +3,18 @@ from get_cve import get_cve
 from time import sleep
 from db import vulnerabilities_collection
 
+query = {
+    "$or": [
+        {"cvss": None},
+        {"epss": None}
+    ]
+}
+
 def enchance_data():
-    cve_ids = vulnerabilities_collection.distinct("cve_id", {"cve_id": {"$ne": None}})
+    #cve_ids = vulnerabilities_collection.distinct("cve_id", {"cve_id": {"$ne": None}})
+    cve_ids = vulnerabilities_collection.distinct("cve_id", query)
+    cve_ids = [cve_id for cve_id in cve_ids if isinstance(cve_id, str) and cve_id.startswith("CVE")]
+    
     print(cve_ids)
     total_cves = len(cve_ids)
     for index, cve_id in enumerate(cve_ids, start=1):
@@ -13,10 +23,11 @@ def enchance_data():
             epss_data = get_epss(cve_id)
             print(f"Processing {index}/{total_cves}: {cve_id}")
             sleep(1)
-            vulnerabilities_collection.update_many(
-                {"cve_id": cve_id},
-                {"$set": {
-                    "cvss": cve_data.get("baseScore"),
-                    "epss": epss_data.get("epss")
+            if cve_data and epss_data:
+                vulnerabilities_collection.update_many(
+                    {"cve_id": cve_id},
+                    {"$set": {
+                        "cvss": cve_data.get("baseScore", 0),
+                        "epss": epss_data.get("epss", 0)
                 }}
             )
